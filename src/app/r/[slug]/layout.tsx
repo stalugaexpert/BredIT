24598ffa6f0291230f44d83b884793bfac976b79
stalugaexpert <stalegapawel@gmail.com>
2023-06-18 19:14:18 +0200

    Added editor with a lot of features, including adding images, links, etc.  using editorjs, added create post functionality.

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,7 +1,9 @@
 import SubscribeLeaveToggle from '@/components/SubscribeLeaveToggle'
+import { buttonVariants } from '@/components/ui/Button'
 import { getAuthSession } from '@/lib/auth'
 import { db } from '@/lib/db'
 import { format } from 'date-fns'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
 const Layout = async ({
@@ -91,6 +93,18 @@ const Layout = async ({
                   subredditName={subreddit.name}
                   isSubscribed={isSubscribed}
                 />
+              ) : null}
+
+              {isSubscribed ? (
+                <Link
+                  className={buttonVariants({
+                    variant: 'outline',
+                    className: 'w-full mb-6',
+                  })}
+                  href={`r/${slug}/submit`}
+                >
+                  Create Post
+                </Link>
               ) : null}
             </dl>
           </div>
---
+++ src/components/Editor.tsx
@@ -0,0 +1,214 @@
+'use client'
+
+import { PostCreationRequest, PostValidator } from '@/lib/validators/post'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import TextAreaAutosize from 'react-textarea-autosize'
+import { zodResolver } from '@hookform/resolvers/zod'
+import type EditorJS from '@editorjs/editorjs'
+import { uploadFiles } from '@/lib/uploadthing'
+import { toast } from '@/hooks/use-toast'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter, usePathname } from 'next/navigation'
+
+interface EditorProps {
+  subredditId: string
+}
+
+const Editor: FC<EditorProps> = ({ subredditId }) => {
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostCreationRequest>({
+    resolver: zodResolver(PostValidator),
+    defaultValues: {
+      subredditId,
+      title: '',
+      content: null,
+    },
+  })
+
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  const ref = useRef<EditorJS>()
+  const _titleRef = useRef<HTMLTextAreaElement>(null)
+
+  const initializeEditor = useCallback(async () => {
+    const EditorJS = (await import('@editorjs/editorjs')).default
+    const Header = (await import('@editorjs/header')).default
+    const Embed = (await import('@editorjs/embed')).default
+    const Table = (await import('@editorjs/table')).default
+    const List = (await import('@editorjs/list')).default
+    const Code = (await import('@editorjs/code')).default
+    const LinkTool = (await import('@editorjs/link')).default
+    const InlineCode = (await import('@editorjs/inline-code')).default
+    const ImageTool = (await import('@editorjs/image')).default
+
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: 'editor',
+        onReady() {
+          ref.current = editor
+        },
+        placeholder: 'Type here to write your post...',
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: '/api/link',
+            },
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              uploader: {
+                async uploadByFile(file: File) {
+                  const [res] = await uploadFiles([file], 'imageUploader')
+
+                  return {
+                    success: 1,
+                    file: {
+                      url: res.fileUrl,
+                    },
+                  }
+                },
+              },
+            },
+          },
+          code: Code,
+          embed: Embed,
+          table: Table,
+          list: List,
+          inlineCode: InlineCode,
+        },
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMounted(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (Object.keys(errors).length) {
+      for (const [_key, value] of Object.entries(errors)) {
+        toast({
+          title: 'Something went wrong',
+          description: (value as { message: string }).message,
+          variant: 'destructive',
+        })
+      }
+    }
+  }, [errors])
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor()
+
+      setTimeout(() => {
+        _titleRef.current?.focus()
+      }, 0)
+    }
+
+    if (isMounted) {
+      init()
+
+      return () => {
+        ref.current?.destroy()
+        ref.current = undefined
+      }
+    }
+  }, [isMounted, initializeEditor])
+
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({
+      title,
+      content,
+      subredditId,
+    }: PostCreationRequest) => {
+      const payload: PostCreationRequest = {
+        title,
+        content,
+        subredditId,
+      }
+
+      const { data } = await axios.post('/api/subreddit/post/create', payload)
+
+      return data
+    },
+    onError: () => {
+      return toast({
+        title: 'Something went wrong',
+        description: 'Your post was not published, please try again later.',
+        variant: 'destructive',
+      })
+    },
+    onSuccess: () => {
+      const newPathname = pathname.split('/').slice(0, -1).join('/')
+
+      router.push(newPathname)
+      router.refresh()
+
+      return toast({
+        description: 'Your post has been published.',
+      })
+    },
+  })
+
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await ref.current?.save()
+
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subredditId: subredditId,
+    }
+
+    createPost(payload)
+  }
+
+  if (!isMounted) {
+    return null
+  }
+
+  const { ref: titleRef, ...rest } = register('title')
+
+  return (
+    <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
+      <form
+        className="w-fit"
+        onSubmit={handleSubmit(onSubmit)}
+        id="subreddit-post-form"
+      >
+        <div className="prose prose-stone dark:prose-invert">
+          <TextAreaAutosize
+            ref={(e) => {
+              titleRef(e)
+              // @ts-ignore
+              _titleRef.current = e
+            }}
+            {...rest}
+            className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
+            placeholder="Title"
+          />
+          <div
+            className="min-h-[500px]"
+            id="editor"
+          />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Editor
