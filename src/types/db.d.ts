ce72cc6656019d50eb1480590bccddb37b05a7ac
stalugaexpert <stalegapawel@gmail.com>
2023-06-18 23:36:29 +0200

    Added Post component, added displaying some posts on subreddit, implemented basic infinity scroll for the PostFeed component.

---
+++ src/types/db.d.ts
@@ -0,0 +1,8 @@
+import { Post, Subreddit, Vote, User, Comment } from '@prisma/client'
+
+export type ExtendedPost = Post & {
+  subreddit: Subreddit
+  votes: Vote[]
+  author: User
+  comments: Comment[]
+}
